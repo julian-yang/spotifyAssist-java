f0fce3128e42f922b0f557aea74237fe78f36b33
julian <yang.julian@gmail.com>
2018-05-16 20:15:11 -0700

    Add support for differentiating between intents.

--- src/main/java/SpotifyAssistDialogflow.java
+++ src/main/java/SpotifyAssistDialogflow.java
@@ -1,28 +1,51 @@
 import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.Map;
 import java.util.function.Function;
 import com.tmsdurham.dialogflow.*;
 import com.tmsdurham.actions.*;
+import java.util.logging.Logger;
+import kotlin.jvm.functions.Function1;
 import main.java.com.tmsdurham.dialogflow.sample.DialogflowAction;
 import spark.Request;
 import spark.Response;
 
 public class SpotifyAssistDialogflow {
-  private static Map<String, Function<DialogflowApp, Void>> intentMap =
-      ImmutableMap.<String, Function<DialogflowApp, Void>>builder()
-          .put("WELCOME", (app) -> welcome(app))
+
+  private static final Logger logger = Logger.getLogger("SpotifyAssistDialogflow");
+
+  enum Intent {
+    WELCOME("input.welcome"),
+    ENABLE_SHUFFLE("playback.shuffle.on");
+
+    private final String intentName;
+
+    Intent(String name) {
+      this.intentName = name;
+    }
+  }
+
+  private static Map<String, Function1<DialogflowApp, Void>> intentMap =
+      ImmutableMap.<String, Function1<DialogflowApp, Void>>builder()
+          .put(Intent.WELCOME.intentName, SpotifyAssistDialogflow::welcome)
+          .put(Intent.ENABLE_SHUFFLE.intentName, SpotifyAssistDialogflow::enableShuffle)
           .build();
 
   static Void welcome(DialogflowApp app) {
-    ResponseWrapper<DialogflowResponse> response = app.tell("Hello world!");
+    ResponseWrapper<DialogflowResponse> response = app.ask("Hello world?");
+    return null;
+  }
+
+  static Void enableShuffle(DialogflowApp app) {
+    app.tell("Turning on shuffle");
     return null;
   }
 
   public static String handlePost(Request request, Response response) throws IOException {
-
     DialogflowAction action = new DialogflowAction(request.raw(), response.raw());
-    ResponseWrapper<DialogflowResponse> responseWrapper = action.getApp().tell("Test123");
+    action.handleRequest(intentMap);
+//    ResponseWrapper<DialogflowResponse> responseWrapper = action.getApp().tell("Test123");
     response.raw().flushBuffer();
     return "noop";
     //    action.handleRequest(intentMap);
